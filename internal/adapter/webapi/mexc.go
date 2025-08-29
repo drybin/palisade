e187c0c6dd2e09572d8a6fed621cac690a65a9c1
Dmitry Rybin <d.rybin@office.ngs.ru>
2025-08-29 22:44:07 +0700

    Tmp commit2

--- internal/adapter/webapi/mexc.go
+++ internal/adapter/webapi/mexc.go
@@ -1,30 +1,30 @@
 package webapi
 
 import (
-	"context"
-	"fmt"
-
-	"github.com/drybin/palisade/pkg/mexcsdk"
-
-	"github.com/drybin/palisade/internal/app/cli/config"
+    "context"
+    "fmt"
+    
+    "github.com/drybin/palisade/pkg/mexc-sdk/mexcsdk"
+    
+    "github.com/drybin/palisade/internal/app/cli/config"
 )
 
 type MexcWebapi struct {
-	config config.MexcConfig
+    config config.MexcConfig
 }
 
 func NewMexcWebapi(
-	config config.MexcConfig,
+    config config.MexcConfig,
 ) *MexcWebapi {
-	return &MexcWebapi{
-		config: config,
-	}
+    return &MexcWebapi{
+        config: config,
+    }
 }
 
 func (m *MexcWebapi) GetBalance(ctx context.Context) error {
-
-	spot := mexcsdk.NewSpot(m.config.ApiKey, m.config.Secret)
-	res := spot.AccountInfo()
-	fmt.Printf("AccountInfo: %+v\n", res)
-	return nil
+    
+    spot := mexcsdk.NewSpot(m.config.ApiKey, m.config.Secret)
+    res := spot.AccountInfo()
+    fmt.Printf("AccountInfo: %+v\n", res)
+    return nil
 }
--- internal/app/cli/registry/container.go
+++ internal/app/cli/registry/container.go
@@ -1,35 +1,35 @@
 package registry
 
 import (
-    repo "github.com/drybin/palisade/internal/adapter/webapi"
-    "github.com/drybin/palisade/internal/app/cli/config"
-    "github.com/drybin/palisade/internal/app/cli/usecase"
-    "github.com/drybin/palisade/pkg/logger"
+	repo "github.com/drybin/palisade/internal/adapter/webapi"
+	"github.com/drybin/palisade/internal/app/cli/config"
+	"github.com/drybin/palisade/internal/app/cli/usecase"
+	"github.com/drybin/palisade/pkg/logger"
 )
 
 type Container struct {
-    Logger   logger.ILogger
-    Usecases *Usecases
-    Clean    func()
+	Logger   logger.ILogger
+	Usecases *Usecases
+	Clean    func()
 }
 
 type Usecases struct {
-    HelloWorld      *usecase.HelloWorld
-    PalisadeProcess *usecase.PalisadeProcess
+	HelloWorld      *usecase.HelloWorld
+	PalisadeProcess *usecase.PalisadeProcess
 }
 
 func NewContainer(
-    config *config.Config,
+	config *config.Config,
 ) (*Container, error) {
-    
-    container := Container{
-        Usecases: &Usecases{
-            HelloWorld:      usecase.NewHelloWorldUsecase(),
-            PalisadeProcess: usecase.NewPalisadeProcessUsecase(repo.NewMexcWebapi(config.MexcConfig)),
-        },
-        Clean: func() {
-        },
-    }
-    
-    return &container, nil
+
+	container := Container{
+		Usecases: &Usecases{
+			HelloWorld:      usecase.NewHelloWorldUsecase(),
+			PalisadeProcess: usecase.NewPalisadeProcessUsecase(repo.NewMexcWebapi(config.MexcConfig)),
+		},
+		Clean: func() {
+		},
+	}
+
+	return &container, nil
 }
