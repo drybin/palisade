b3b1d29fab98fd598ecbe5455ad54178cc9be9d4
Dmitry Rybin <dmitriy.rybin@shkulev.ru>
2026-08-21 10:58:18 +0700

    Improve run script

--- run-cron.sh
+++ run-cron.sh
@@ -2,14 +2,14 @@
 
 cd /root/palisade || exit 1
 
-/usr/bin/timeout --signal=TERM --kill-after=30s 3m \
+/usr/bin/timeout --signal=TERM --kill-after=300s 3m \
     ./palisade process \
     >> /root/palisade/process_log 2>&1
 
-/usr/bin/timeout --signal=TERM --kill-after=30s 3m \
+/usr/bin/timeout --signal=TERM --kill-after=300s 3m \
     ./palisade process-sell \
     >> /root/palisade/process_sell_log 2>&1
 
-/usr/bin/timeout --signal=TERM --kill-after=30s 3m \
+/usr/bin/timeout --signal=TERM --kill-after=300s 3m \
     ./palisade paper-palisade-signals \
     >> /root/palisade/paper_palisade_signals_log 2>&1
